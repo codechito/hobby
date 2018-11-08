d24c893d9235bf28f95fbee2b08b0c84badf0255
codechito <chito@burstsms.com>
2018-11-08 08:24:56 +0800

    change hook to sharky downloaded using npm

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var emitter = require('./core/hooks');
+var emitter = require('sharky');
 var mongo = require('./core/mongo')(emitter);
 var item = require('./routes/item')(emitter);
 
